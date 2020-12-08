3a4059ae8b1649daee8804f86f5f06baa267e365
Saul-Ponce <saulponce1698@hotmail.com>
2020-12-08 16:47:32 -0600

    ejercicio 2

--- MetodosProbabilisticos2020/src/ejemplo10/Ej10.java
+++ MetodosProbabilisticos2020/src/ejemplo10/Ej10.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplo11;
+package ejemplo10;
 
 import static java.lang.Math.pow;
 
@@ -11,7 +11,7 @@ import static java.lang.Math.pow;
  *
  * @author Saul Ponce
  */
-public class Ej11 {
+public class Ej10 {
 
     /**
      * @param args the command line arguments
